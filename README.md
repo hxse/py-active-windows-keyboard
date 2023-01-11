e2efaaca77977c01108a4e5cd817c38c8c0fb7f6
hxse <narutohy@163.com>
2023-01-11 21:18:05 +0800

    update args and config

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # py-active-windows-keyboard
   * download https://github.com/libusb/hidapi/releases
-  * add path: `ctypes.CDLL(r"D:\App\app\hidapi\hidapi-win\x64\hidapi.dll")`
-  * create config.json
-  *
-    ```
-    {
-    "EP_SIZE": 32,
-    "vid": "0x45d4",
-    "pid": "0x0911",
-    "usage_page": "0xFF60",
-    "usage_id": "0x61",
-    "sleepTime": 0.2,
-    "sendList": [1, 5]
-    }
-    ```
+  * 方式一,通过配置文件
+    * `pdm run python .\main.py config ".\config.json"`
+    * create config.json
+    *
+      ```
+      {
+        "sendList": [1, 5],
+        "vid": "0x45d4",
+        "pid": "0x0911",
+        "usage_page": "0xFF60",
+        "usage_id": "0x61",
+        "dllPath": "D:\\App\\app\\hidapi\\hidapi-win\\x64\\hidapi.dll",
+        "EP_SIZE": 32,
+        "sleepTime": 0.2
+      }
+      ```
+    * sendList字段,是个数组,0-255范围,在示例中的第一个数字表达模式,第二个数字表达切换图层
+  * 方式二,通过参数
+  * `pdm run python .\main.py args [1,5]  0x45d4 0x0911 0xFF60 0x61 "D:\App\app\hidapi\hidapi-win\x64\hidapi.dll" 32 0.2`
--- main.py
+++ main.py
@@ -3,28 +3,31 @@
 """
 https://www.reddit.com/r/olkb/comments/x00jyo/cannot_send_or_receive_data_with_raw_hid_feature/
 """
-import ctypes
-
-ctypes.CDLL(r"D:\App\app\hidapi\hidapi-win\x64\hidapi.dll")
-
-import hid
 import time
 import json
+import fire
+import ctypes
 
-with open("config.json", "r", encoding="utf-8") as file:
-    config = json.load(file)
-    for i in ["vid", "pid", "usage_page", "usage_id"]:
-        c = config[i]
-        if type(c) == str:
-            config[i] = int(c, 16)
-    EP_SIZE = config["EP_SIZE"]  # ATMEGA32U4通常需要32字节大小,超出会自动截断 #更新,好像不用这个也行,不确定
-    vid = config["vid"]
-    pid = config["pid"]
-    usage_page = config["usage_page"]
-    usage_id = config["usage_id"]
 
-    sendList = config["sendList"]  # bytes([0,1])  or b'hellow world'
-    sleepTime = config["sleepTime"]
+def get_config(path):
+    with open(path, "r", encoding="utf-8") as file:
+        config = json.load(file)
+        for i in ["vid", "pid", "usage_page", "usage_id"]:
+            c = config[i]
+            if type(c) == str:
+                config[i] = int(c, 16)
+        return {
+            "EP_SIZE": config[
+                "EP_SIZE"
+            ],  # ATMEGA32U4通常需要32字节大小,超出会自动截断 #更新,好像不用这个也行,不确定
+            "vid": config["vid"],
+            "pid": config["pid"],
+            "usage_page": config["usage_page"],
+            "usage_id": config["usage_id"],
+            "sendList": config["sendList"],
+            "sleepTime": config["sleepTime"],
+            "dllPath": config["dllPath"],
+        }
 
 
 def pad_message(payload: bytes) -> bytes:
@@ -37,33 +40,96 @@ def to_bytes(data: str) -> bytes:
     return data.encode()
 
 
-keyboard: hid.Device = None
+def find_device(vid, pid, usage_page, usage_id):
+    for device in hid.enumerate():
+        if (
+            device["vendor_id"] == vid
+            and device["product_id"] == pid
+            and device["usage_page"] == usage_page
+            and device["usage"] == usage_id
+        ):
+            keyboard = hid.Device(path=device["path"])
+            return keyboard
 
-for device in hid.enumerate():
-    if (
-        device["vendor_id"] == vid
-        and device["product_id"] == pid
-        and device["usage_page"] == usage_page
-        and device["usage"] == usage_id
-    ):
-        keyboard = hid.Device(path=device["path"])
 
-if keyboard == None:
-    print("Keyboard was not found.")
-    exit(1)
+def send_device(sendList, keyboard):
+    message = pad_message(bytes(sendList))
+    message = (
+        b"\x00" + message[0 : EP_SIZE - 1]
+    )  # 因为第0个比特会被 raw_hid_receive 吞掉,所以用第1个位置的比特
+    print("sending:", message, len(message))
+    keyboard.write(message)
+    print(f"reading... wait {sleepTime} second")
+    time.sleep(sleepTime)
+    received = keyboard.read(EP_SIZE)
+    print("received:", received, len(received))
+    keyboard.close()
+    print("device closed")
 
-print("Keyboard found!")
-print("Product:", keyboard.product)
-print("Manufacturer:", keyboard.manufacturer)
 
-message = pad_message(bytes(sendList))
-message = b"\x00" + message[0 : EP_SIZE - 1]  # 因为第0个比特会被 raw_hid_receive 吞掉,所以用第1个位置的比特
-print("sending:", message, len(message))
-keyboard.write(message)
-print(f"reading... wait {sleepTime} second")
-time.sleep(sleepTime)
-received = keyboard.read(EP_SIZE)
-print("received:", received, len(received))
+def args(
+    sendList,
+    vid,
+    pid,
+    usage_page,
+    usage_id,
+    dllPath,
+    EP_SIZE=32,
+    sleepTime=0.2,
+):
+    main(
+        sendList,
+        vid,
+        pid,
+        usage_page,
+        usage_id,
+        dllPath,
+        EP_SIZE,
+        sleepTime,
+    )
 
-keyboard.close()
-print("device closed")
+
+def config(configPath):
+    config = get_config(configPath)
+    main(
+        config["sendList"],
+        config["vid"],
+        config["pid"],
+        config["usage_page"],
+        config["usage_id"],
+        config["dllPath"],
+        config["EP_SIZE"],
+        config["sleepTime"],
+    )
+
+
+def main(
+    sendList,
+    vid,
+    pid,
+    usage_page,
+    usage_id,
+    dllPath,
+    _EP_SIZE,
+    _sleepTime,
+):
+
+    global hid, EP_SIZE, sleepTime
+    ctypes.CDLL(dllPath)
+    import hid as _hid
+
+    hid = _hid
+    EP_SIZE = _EP_SIZE
+    sleepTime = _sleepTime
+
+    keyboard = find_device(vid, pid, usage_page, usage_id)
+    if keyboard:
+        print("Product:", keyboard.product)
+        print("Manufacturer:", keyboard.manufacturer)
+        send_device(sendList, keyboard)
+    else:
+        print("Keyboard was not found.")
+
+
+if __name__ == "__main__":
+    fire.Fire({"args": args, "config": config})
